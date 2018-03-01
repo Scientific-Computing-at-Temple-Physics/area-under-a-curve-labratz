e4c16092ad093cdadd57c53e3b9ba2e7fb687cef
John Doe <jdgblackfox@gmail.com>
2018-03-01 18:16:06 -0500

    Got the theoretical stuff down

--- curvearea.py
+++ curvearea.py
@@ -66,10 +66,30 @@ for xpos in range(num_steps):
 
 print("We calculated an area under the curve of "+str(straightcomp)+ " for the straight line, an area of "+str(paracomp)+" for the parabola, and an area of "+str(exponencomp)+" for the more complicated exponential function.")
 
+#Integral based computation
+def integrate_straight(xs,ms,bs):
+    ys=(float(ms)/2)*float(xs)**2+float(bs)*float(xs)
+    return ys
 
+def integrate_para(xp,ap,bp,cp):
+    yp=(float(ap)/3)*float(xp)**3+(float(bp)/2)*float(xp)**2+float(cp)*float(xp)
+    return yp
 
+def integrate_exp(xe,ae,be,ce):
+    dv=ma.exp(-float(ce)*float(xe)) #bunch of unnecessary variables in here, simply for checking
+    v=-(1/float(ce))*ma.exp(-float(ce)*float(xe))
+    u=(float(ae)*float(xe)+float(be))
+    du=float(ae)
+    vdu=-ma.exp(-float(ce)*float(xe))
+    intvdu=(1/float(ce))*ma.exp(-float(ce)*float(xe))
+    ye=(u*v)-intvdu
+    return ye
 
+theory_str=integrate_straight(end,mst,bst)-integrate_straight(start,mst,bst)
+theory_para=integrate_para(end,apa,bpa,cpa)-integrate_para(start,apa,bpa,cpa)
+theory_exp=integrate_exp(end,aex,bex,cex)-integrate_exp(start,aex,bex,cex)
 
+print("Theoretically, the area under the straight line should be "+str(theory_str)+", the area under the parabola should be "+str(theory_para)+", and the area under the more complicated exponential curve should be " +str(theory_exp))
 
 
 
